e29286cb83ad567afb7938354d1e962ed0477a05
Connor Boesch <connor_boesch@yahoo.com>
2016-04-30 00:02:49 -0500

    Updated app to add functionality.

--- Tic-Tac-Toe/M/BoardManager.cs
+++ Tic-Tac-Toe/M/BoardManager.cs
@@ -1,24 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tic_Tac_Toe.M {
+﻿namespace Tic_Tac_Toe.M {
     class BoardManager {
-        //Holds global values such as board tile grid .  Currently pretty useless.
         public static uint tileColumns;
         public static uint tileRows;
+        public Board board;
 
         public BoardManager() {
-            //Can we do better than hard coding a convenient default for us?
-            //What do we do if params unknown?
+            //Hard coded to setup 3x3, can be overloaded
             tileColumns=3;
             tileRows=3;
+            board=new Board(tileColumns,tileRows);
         }
         public BoardManager(uint columnParam, uint rowParam){
             tileColumns=columnParam;
             tileRows=rowParam;
+            board=new Board(columnParam,rowParam);
+        }
+
+        public Tile getTile(string location) {
+            return board.getTile(location);
+        }
+
+        public bool isWinner() {
+            return board.isWinner();
+        }
+
+        public void Reset() {
+            board=new Board(3,3);
         }
     }
 }
--- Tic-Tac-Toe/M/Tile.cs
+++ Tic-Tac-Toe/M/Tile.cs
@@ -1,26 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
+﻿using System.Drawing;
 namespace Tic_Tac_Toe.M {
     class Tile {
-        //Player can only be set by a player if it is initially null
-        public Player Player {
-            get { return this.Player; }
-            set {
-                if(this.Player==null)
-                    this.Player=value;
-                else
-                    return; //Throw some sort of error?  Cannot overwrite tile as player
-            }
-        }
-        public Color Color { get; private set; } 
+        public Player Player {get; set;}
+        public Color Color {get;set;} 
 
         public Tile() {
-            this.Player=null;
+            this.Player=new Player("EMPTY",Color.White,'-');
             this.Color=Color.White;
+        }
+
+        public Tile(Player player, Color color) {
+            Player=player;
+            Color=color;
         }
     }
 }
